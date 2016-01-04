fe8a787a2cc60285deeed0ff9edc024536544799
bytecats <bytecats@gmail.com>
2016-01-04 11:20:09 +0800

    del files

--- vue/app/component/id.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<input type="hidden" name="{{postId}}">
-</template>
-<script>
-module.exports = {
-  props:['postId']
-
-}
-</script>
--- vue/app/component/loading.vue
+++
@@ -1,3 +0,0 @@
-<template>
-	<div id="loading" class="ui-loading-block"><div class="ui-loading"></div></div>
-</template>
